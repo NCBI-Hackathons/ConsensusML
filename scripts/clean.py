3d11a27f29e38c739367181683b3302172ed61c7
David Lee <davidlee327@gmail.com>
2019-02-04 21:09:25 -0800

    create cleaning functions for transposing assay and merging with clinical data

--- scripts/clean.py
+++ scripts/clean.py
@@ -15,4 +15,26 @@ def manifest_clinical_merge(manifest_df, clinical_df, target):
     final_df = clinical_df.merge(target_df, on='TARGET USI')
     return final_df
 
-    
+
+def assay_transpose(assay_df):
+    """
+    input assay_df
+    transposes df so that rows are patient assays
+    """
+    assay_pt_df = assay_df.T
+    assay_pt_df.columns = assay_pt_df.iloc[0]
+    assay_pt_df = assay_pt_df.iloc[1:]
+    assay_pt_df['TARGET USI'] = list(assay_pt_df.index)
+    assay_pt_df['TARGET USI'] = assay_pt_df['TARGET USI'].apply(lambda x: x[0:16])
+    assay_pt_df['2nd ID'] = list(assay_pt_df.index)
+    assay_pt_df['2nd ID'] = assay_pt_df['2nd ID'].apply(lambda x: x[16:])
+    return assay_pt_df
+
+def assay_clinical_merge(assay_df, clinical_df):
+    """
+    input assay df, clinical df, and disease (AML, WT, or NBL)
+    return merged data
+    """
+    clinical_df['TARGET USI'] = clinical_df['TARGET USI'].apply(lambda x: x.replace('-', '.'))
+    df = clinical_df.merge(assay_df, how='left', on='TARGET USI')
+    return df
