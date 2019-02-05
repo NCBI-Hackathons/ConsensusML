bbb3d0844e9af810124822d28bf4f71b6fe50363
David Lee <davidlee327@gmail.com>
2019-02-05 11:31:39 -0800

    write new function to reorder columns and fix bug for id matching

--- scripts/clean.py
+++ scripts/clean.py
@@ -24,17 +24,32 @@ def assay_transpose(assay_df):
     assay_pt_df = assay_df.T
     assay_pt_df.columns = assay_pt_df.iloc[0]
     assay_pt_df = assay_pt_df.iloc[1:]
-    assay_pt_df['TARGET USI'] = list(assay_pt_df.index)
-    assay_pt_df['TARGET USI'] = assay_pt_df['TARGET USI'].apply(lambda x: x[0:16])
-    assay_pt_df['2nd ID'] = list(assay_pt_df.index)
-    assay_pt_df['2nd ID'] = assay_pt_df['2nd ID'].apply(lambda x: x[16:])
+    assay_pt_df['entity_submitter_id'] = list(assay_pt_df.index)
+    # assay_pt_df['TARGET USI'] = list(assay_pt_df.index)
+    # assay_pt_df['TARGET USI'] = assay_pt_df['TARGET USI'].apply(lambda x: x[0:16])
+    # assay_pt_df['2nd ID'] = list(assay_pt_df.index)
+    # assay_pt_df['2nd ID'] = assay_pt_df['2nd ID'].apply(lambda x: x[16:])
     return assay_pt_df
+
+def column_reorder(clinical_df):
+    columns = clinical_df.columns
+    reorder_columns = [columns[0], columns[-1]]
+    for column in columns[1:-1]:
+        reorder_columns.append(column)
+    return reorder_columns
+
 
 def assay_clinical_merge(assay_df, clinical_df):
     """
     input assay df, clinical df, and disease (AML, WT, or NBL)
     return merged data
     """
-    clinical_df['TARGET USI'] = clinical_df['TARGET USI'].apply(lambda x: x.replace('-', '.'))
-    df = clinical_df.merge(assay_df, how='left', on='TARGET USI')
+    clinical_df['entity_submitter_id'] = clinical_df['entity_submitter_id'].apply(lambda x: x.replace('-', '.'))
+    # clinical_df['TARGET USI'] = clinical_df['TARGET USI'].apply(lambda x: x.replace('-', '.'))
+    # df = clinical_df.merge(assay_df, how='left', on='TARGET USI')
+    df = clinical_df.merge(assay_df, how='left', on='entity_submitter_id')
+    df['Diagnostic ID'] = df.loc[:, 'entity_submitter_id'].apply(lambda x: x[-7:-4])
+    columns = column_reorder(df)
+    df = df[columns]
+
     return df
