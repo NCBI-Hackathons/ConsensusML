218f357d7c0be64015e1ee90ae438910359e8a45
David Lee <davidlee327@gmail.com>
2019-02-05 18:15:21 -0800

    write new functions for pipelining model building

--- scripts/model_comp.py
+++ scripts/model_comp.py
@@ -3,33 +3,34 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import  RandomForestClassifier, GradientBoostingClassifier
 from xgboost import XGBClassifier
 
-# def model_comp(df):
-#     xgboost_model = XGBClassifier(learning_rate = 0.01, max_depth = 3, n_estimators = 300, random_state=8)
-#     gradient_boost_model = GradientBoostingClassifier(learning_rate=0.01, max_depth=4, max_features='log2', min_samples_leaf=4, n_estimators=280, subsample=0.25, random_state=8)
-#     random_forest_model = RandomForestClassifier(n_estimators=300, max_depth=3, verbose=1, random_state=8)
+def model_comp(df, X_train, X_test, y_train, y_test):
+    xgboost_model = XGBClassifier(learning_rate = 0.01, max_depth = 3, n_estimators = 300, random_state=8)
+    gradient_boost_model = GradientBoostingClassifier(learning_rate=0.01, max_depth=4, max_features='log2', min_samples_leaf=4, n_estimators=280, subsample=0.25, random_state=8)
+    random_forest_model = RandomForestClassifier(n_estimators=300, max_depth=3, verbose=1, random_state=8)
 
-#     xgboost_model.fit(X_train, y_train)
-#     gradient_boost_model.fit(X_train, y_train)
-#     random_forest_model.fit(X_train, y_train)
+    xgboost_model.fit(X_train, y_train)
+    gradient_boost_model.fit(X_train, y_train)
+    random_forest_model.fit(X_train, y_train)
 
-#     p_random_forest = random_forest_model.predict_proba(X_test)
-#     p_gradient_boost =  gradient_boost_model.predict_proba(X_test)
-#     p_xgboost = xgboost_model.predict_proba(X_test)
+    p_random_forest = random_forest_model.predict_proba(X_test)
+    p_gradient_boost =  gradient_boost_model.predict_proba(X_test)
+    p_xgboost = xgboost_model.predict_proba(X_test)
 
-#     #need to concat these two arrays somehow...
-#     ensemble_p_0 = (p_random_forest[:,0] + p_gradient_boost[:,0] + p_xgboost[:,0])/3
-#     ensemble_p_1 = (p_random_forest[:,1] + p_gradient_boost[:,1] + p_xgboost[:,1])/3
-#     # ensemble_p = np.concatenate(ensemble_p_0, ensemble_p_1, axis=0)
+    #need to concat these two arrays somehow...
+    ensemble_p_0 = (p_random_forest[:,0] + p_gradient_boost[:,0] + p_xgboost[:,0])/3
+    ensemble_p_1 = (p_random_forest[:,1] + p_gradient_boost[:,1] + p_xgboost[:,1])/3
+    # ensemble_p = np.concatenate(ensemble_p_0, ensemble_p_1, axis=0)
 
-#     random_forest_ll = log_loss(y_test, p_random_forest)
-#     gradient_boost_ll = log_loss(y_test, p_gradient_boost)
-#     xgboost_ll = log_loss(y_test, p_xgboost)
-#     ensemble_ll = log_loss(y_test, ensemble_p_1)
+    random_forest_ll = log_loss(y_test, p_random_forest)
+    gradient_boost_ll = log_loss(y_test, p_gradient_boost)
+    xgboost_ll = log_loss(y_test, p_xgboost)
+    ensemble_ll = log_loss(y_test, ensemble_p_1)
                                         
 
-#     print("Ensemble Log Loss " + str(ensemble_ll))
-#     print("Gradient Boost Log Loss " + str(gradient_boost_ll))
-#     print("Random Forest Log Loss " + str(random_forest_ll))
+    print("Ensemble Log Loss " + str(ensemble_ll))
+    print("Gradient Boost Log Loss " + str(gradient_boost_ll))
+    print("Random Forest Log Loss " + str(random_forest_ll))
+    print("XGBoost Log Loss " + str(xgboost_ll))
 
 
 def data_prep_columns(df, var):
@@ -42,6 +43,7 @@ def data_prep_columns(df, var):
 
     Returns
     ----------------
+    dataframe
     columns of either most/least variance from the two subgroups
     """
     if var =='Max':
@@ -71,7 +73,7 @@ def data_prep_columns(df, var):
     high_columns_to_take_full = [item + 84 for item in high_columns_to_take]
 
     low_columns_to_take_full.extend(x for x in high_columns_to_take_full if x not in low_columns_to_take_full)
-    return low_columns_to_take_full
+    return df2, low_columns_to_take_full
 
 def model_prep(df, columns_to_take):
     data = df.iloc[:, columns_to_take]
