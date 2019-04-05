7389f3516264b5fdcaf3b9008d27697581c3316a
David Lee <davidlee327@gmail.com>
2019-04-04 17:02:30 -0700

    clean functions, create notebook to test

--- scripts/cm_work/data_prep.py
+++ scripts/cm_work/data_prep.py
@@ -10,7 +10,10 @@ from sklearn.model_selection import permutation_test_score
 import datetime
 
 def create_x_y_data():
-    """function for creating pre-chosen x_train and y_train"""
+    """
+    function for creating pre-chosen x_train and y_train
+    for permutation tests
+    """
     df = pd.read_csv('deg.csv')
     df.drop(columns=['Unnamed: 0'], inplace=True)
 
@@ -35,3 +38,33 @@ def create_x_y_data():
     y = df.iloc[:, -1]
 
     return X_train, y_train, X_test, y_test, X, y
+
+def cross_val_data(csv):
+    """
+    function for preparing data for kfold validation
+
+    inputs
+    ------
+    csv: csv path as string
+
+    returns
+    -------
+    X: dataframe to train on
+    y: labels
+    final_X: validation dataframe
+    final_y: validation labels
+    """
+    df = pd.read_csv('deg.csv')
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+    columns = df.columns[1:-1]
+
+    final_validation = df.sample(frac=0.1)
+
+    final_X = final_validation[columns]
+    final_y = final_validation.iloc[:, -1]
+
+    data = df.drop(index= final_validation.index)
+    X = data[columns]
+    y = data.iloc[:, -1]
+
+    return X, y, final_X, final_y
--- scripts/cm_work/data_viz.py
+++ scripts/cm_work/data_viz.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def histogram_plot(csv, model_type):
+def histogram_plot(csv, model_type, loss='log loss'):
     """
     Function to plot histogram of permutation test
 
@@ -9,16 +9,35 @@ def histogram_plot(csv, model_type):
     -------
     csv: string csv address
     model_type: str (Random Forest or XGBoost)
+    loss: str, type of loss (log loss or accuracy)
 
     returns:
     --------
     visualization 
     """
+
     df = pd.read_csv(csv)
     df.drop(columns=['Unnamed: 0'], inplace=True)
     df.columns = ['real score', 'perm scores', 'p-value']
-    df['perm scores'] = df['perm scores'].apply(lambda x: x * -1.0)
-    df['real score'] = df['real score'].apply(lambda x: x * -1.0)
+
+    if loss == 'log loss':
+        loss = 'Log Loss'
+        df['perm scores'] = df['perm scores'].apply(lambda x: x * -1.0)
+        df['real score'] = df['real score'].apply(lambda x: x * -1.0)
+        actual_score = round(df['real score'].mean(), 3)
+        actual_score_marker = actual_score + 0.1
+        y_ = 50
+        x_ = 0.4
+        y1 = 100
+        x1 = actual_score_marker
+        
+    if loss == 'accuracy':
+        loss = 'Accuracy'
+        y_ = 50
+        x_ = 0.75
+        y1 = 100
+        x1 = 0.75
+        
     actual_score = round(df['real score'].mean(), 3)
     actual_score_marker = actual_score + 0.1
     p_value = round(df['p-value'].mean(), 4)
@@ -27,8 +46,8 @@ def histogram_plot(csv, model_type):
     plt.figure(figsize=(10, 6))
     plt.hist(df['perm scores'], bins=50)
     plt.axvline(x = actual_score, color='r', )
-    plt.title(f'Permutation Test {model_type} Log Loss vs Actual Log Loss')
-    plt.xlabel('Log Loss')
+    plt.title(f'Permutation Test {model_type} {loss} vs Actual {loss}')
+    plt.xlabel(f'{loss}')
     plt.ylabel('Occurences')
-    plt.text(y =50, x = 0.4, s = f'P-value: {p_value}')
-    plt.text(y = 100, x = actual_score_marker, s = f'Actual Log Loss: {actual_score}' );
+    plt.text(y = y_, x = x_, s = f'P-value: {p_value}')
+    plt.text(y = y1, x = x1, s = f'Actual {loss}: {actual_score}' );
